9d93cce6febd576973c33e7eead790a2b23eba66
Ivanica Gabriel <ivanicagabriel@gmail.com>
2012-07-07 21:55:06 +0300

    + gesture not inserted in iframes

--- data/gestures.js
+++ data/gestures.js
@@ -3,20 +3,25 @@
  * Usage	Click on the left edge of the document - pageX = 0
  */
 
-
-if (window.top == window.self) {
-	alert("od");
+if (window.location == window.parent.location) {
+	
+	console.log("blea");
 	
 	document.addEventListener('mousedown', function doSomething(e) {
 		var posx = 0;
-		if (!e) var e = window.event;
+
+		if (!e) 
+			var e = window.event;
+		
 		if (e.pageX || e.pageY) 	{
 			posx = e.pageX;
 		}
+
 		else if (e.clientX || e.clientY) 	{
 			posx = e.clientX + document.body.scrollLeft
 				+ document.documentElement.scrollLeft;
 		}
+
 		if (posx == 0)
 			self.port.emit('show');
 	});	
