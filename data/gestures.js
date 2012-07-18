016a15df3ca011d84c93eebad80561373ee8e376
Ivanica Gabriel <ivanicagabriel@gmail.com>
2012-07-18 23:54:41 +0300

    + add observer

--- data/gestures.js
+++ data/gestures.js
@@ -1,28 +1,11 @@
 /*
  * Add mouse gestures for opening the panel
- * Usage	Click on the left edge of the document - pageX = 0
+ * Usage	Click on the left edge of the document - mouse X position 0
  */
 
 if (window.location == window.parent.location) {
-	
-	console.log("blea");
-	
-	document.addEventListener('mousedown', function doSomething(e) {
-		var posx = 0;
-
-		if (!e) 
-			var e = window.event;
-		
-		if (e.pageX || e.pageY) 	{
-			posx = e.pageX;
-		}
-
-		else if (e.clientX || e.clientY) 	{
-			posx = e.clientX + document.body.scrollLeft
-				+ document.documentElement.scrollLeft;
-		}
-
-		if (posx == 0)
+	document.addEventListener('mousedown', function marksUI_GesturesEvent(e) {
+		if (e.clientX == 0)
 			self.port.emit('show');
 	});	
 }
